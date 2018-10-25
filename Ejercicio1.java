4a52e591556d675eff786c1b826a0ec6344c869c
Necrotype <camiloj0813@hotmail.com>
2018-10-25 18:10:59 -0500

    Add files via upload

---
+++ Ejercicio1.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1;
+
+/**
+ *
+ * @author pc
+ */
+public class Ejercicio1 {
+
+   
+  
+   public static void main(String[] args) {
+
+        int numero1=10; //variables enteras
+        int numero2=5;  //variables enteras
+
+
+ 
+     int suma=numero1+numero2; 
+       System.out.println("la suma es "+suma);//imprime la suma
+     
+     int resta=numero1-numero2;
+      System.out.println("la resta es "+resta);//imprime la resta
+     
+     
+     int division=numero1/numero2;
+     System.out.println("la division es "+division);//imprime la division
+     
+     int multiplicacion=numero1*numero2;
+     System.out.println("la multiplicacion es "+multiplicacion);//imprime la multiplicacion
+    
+}
+} 
